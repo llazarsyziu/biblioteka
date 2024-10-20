89f08b112bdfb999a7394991ad52dba630400b73
Llazar Syziu <llazarsyziu62@gmail.com>
2024-10-20 10:43:51 +0200

    First commit

---
+++ src/main/java/org/example/static_data/RequestStatus.java
@@ -0,0 +1,8 @@
+package org.example.static_data;
+
+public enum RequestStatus {
+    CREATED,
+    COMPLETED,
+    CANCELED
+
+}
---
+++ src/main/java/org/example/static_data/UserRole.java
@@ -0,0 +1,8 @@
+package org.example.static_data;
+
+public enum UserRole {
+    ADMIN,
+    EMPLOYEE,
+    CLIENT
+
+}
